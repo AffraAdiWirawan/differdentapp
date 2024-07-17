a13103ce55cc306fdd808ed8a6b5ca504c49e413
AffraAdiWirawan <127713609+AffraAdiWirawan@users.noreply.github.com>
2024-07-17 22:57:31 +0700

    Update register.dart

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:pkm_mobile/pages/component/bottomnavbar%20.dart';
+import 'package:pkm_mobile/pages/component/bottomnavbar%20.dart'; // Corrected import path
 import 'package:pkm_mobile/pages/login_page.dart';
-import 'package:pkm_mobile/pages/pengingat.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+  String? _NIK, _nama_lengkap, _email, _username, _password;
+  bool _passwordVisible = false;
+
+  Future<void> _register() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      final response = await http.post(
+        Uri.parse('http://loginregister.masjidbaitulhikmah.com/register.php'),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+          'NIK': _NIK!,
+          'namalengkap': _nama_lengkap!,
+          'email': _email!,
+          'username': _username!,
+          'password': _password!,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        if (jsonData['success']) {
+          // Register successful, navigate to login screen
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => LoginPage()),
+          );
+        } else {
+          // Register failed
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(jsonData['message'])),
+          );
+        }
+      } else {
+        // Error occurred
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error occurred')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,108 +62,139 @@ class Register extends StatelessWidget {
       body: Align(
         alignment: Alignment.topCenter,
         child: SingleChildScrollView(
-          child: SizedBox (
-            width: 300,
-            child: Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'NIK'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Masukan Nomor atau Email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                        },
-                      ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 300,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'NIK',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Masukan NIK';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              // Set value for NIK
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Nama Lengkap',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Masukan Nama Lengkap';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              // Set value for Nama Lengkap
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Masukan Email';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              // Set value for Email
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Username',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Masukan Username';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _username = value;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: TextFormField(
+                            obscureText: !_passwordVisible,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: const OutlineInputBorder(),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _passwordVisible = !_passwordVisible;
+                                  });
+                                },
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Masukan Password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _password = value;
+                            },
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: _register, // Call _register method
+                          child: const Text('Register'),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'Nama Lengkap'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Masukan Nomor atau Email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'Email'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Masukan Nomor atau Email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'Username'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Masukan Nomor atau Email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'Password'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Masukan Nomor atau Email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: const InputDecoration(labelText: 'Confirm Password'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Masukan Nomor atau Email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                        },
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Get.to(const LoginPage());
-                      },
-                      child: const Text('Register'),
-                    ),
-                  ],
+                  ),
                 ),
-              )
-          )
+              ),
+            ),
+          ),
         ),
       ),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 }
